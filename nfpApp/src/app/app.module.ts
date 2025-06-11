b1032b55c96e61368bec8535a8c3819834bb8ac8
chavideh <104700811+chavideh@users.noreply.github.com>
2025-06-11 16:41:52 +0330

    to starter

--- nfpApp/src/app/app.module.ts
+++ nfpApp/src/app/app.module.ts
@@ -1,95 +1,63 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpBackend, HttpClient, HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
-// search module
-import { NgPipesModule } from 'ngx-pipes';
+//Routes
+import { routes } from './app.route';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { LayoutsModule} from "./layouts/layouts.module";
-import { PagesModule } from "./pages/pages.module";
-
-// Auth
-import { HttpClient, HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { environment } from '../environments/environment';
-import { initFirebaseBackend } from './authUtils';
-import { FakeBackendInterceptor } from './core/helpers/fake-backend';
-import { ErrorInterceptor } from './core/helpers/error.interceptor';
-import { JwtInterceptor } from './core/helpers/jwt.interceptor';
-
-// Language
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-
-// Store
-import { rootReducer } from './store';
+// store
 import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { EffectsModule } from '@ngrx/effects';
-import { EcommerceEffects } from './store/Ecommerce/ecommerce_effect';
-import { ProjectEffects } from './store/Project/project_effect';
-import { TaskEffects } from './store/Task/task_effect';
-import { CRMEffects } from './store/CRM/crm_effect';
-import { CryptoEffects } from './store/Crypto/crypto_effect';
-import { InvoiceEffects } from './store/Invoice/invoice_effect';
-import { TicketEffects } from './store/Ticket/ticket_effect';
-import { FileManagerEffects } from './store/File Manager/filemanager_effect';
-import { TodoEffects } from './store/Todo/todo_effect';
-import { ApplicationEffects } from './store/Jobs/jobs_effect';
-import { ApikeyEffects } from './store/APIKey/apikey_effect';
-import { AuthenticationEffects } from './store/Authentication/authentication.effects';
+import { indexReducer } from './store/index.reducer';
 
-export function createTranslateLoader(http: HttpClient): any {
-  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+// shared module
+import { SharedModule } from 'src/shared.module';
+
+// i18n
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+// AOT compilation support
+export function HttpLoaderFactory(httpHandler: HttpBackend): TranslateHttpLoader {
+    return new TranslateHttpLoader(new HttpClient(httpHandler));
 }
 
-if (environment.defaultauth === 'firebase') {
-  initFirebaseBackend(environment.firebaseConfig);
-} else {
-  FakeBackendInterceptor;
-}
+// dashboard
+import { IndexComponent } from './index';
 
-@NgModule({ declarations: [
-        AppComponent
-    ],
-    bootstrap: [AppComponent], imports: [TranslateModule.forRoot({
-            defaultLanguage: 'fa',
+// Layouts
+import { AppLayout } from './layouts/app-layout';
+import { AuthLayout } from './layouts/auth-layout';
+
+import { HeaderComponent } from './layouts/header';
+import { FooterComponent } from './layouts/footer';
+import { SidebarComponent } from './layouts/sidebar';
+import { ThemeCustomizerComponent } from './layouts/theme-customizer';
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }),
+        BrowserModule,
+        BrowserAnimationsModule,
+        CommonModule,
+        FormsModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
             loader: {
                 provide: TranslateLoader,
-                useFactory: (createTranslateLoader),
-                deps: [HttpClient]
-            }
+                useFactory: HttpLoaderFactory,
+                deps: [HttpBackend],
+            },
         }),
-        BrowserAnimationsModule,
-        BrowserModule,
-        AppRoutingModule,
-        LayoutsModule,
-        PagesModule,
-        StoreModule.forRoot(rootReducer),
-        StoreDevtoolsModule.instrument({
-            maxAge: 25, // Retains last 25 states
-            logOnly: environment.production, // Restrict extension to log-only mode
-        }),
-        EffectsModule.forRoot([
-            AuthenticationEffects,
-            EcommerceEffects,
-            ProjectEffects,
-            TaskEffects,
-            CRMEffects,
-            CryptoEffects,
-            InvoiceEffects,
-            TicketEffects,
-            FileManagerEffects,
-            TodoEffects,
-            ApplicationEffects,
-            ApikeyEffects
-        ]),
-        NgPipesModule], providers: [
-        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-        { provide: HTTP_INTERCEPTORS, useClass: FakeBackendInterceptor, multi: true },
-        provideHttpClient(withInterceptorsFromDi()),
-    ] })
-export class AppModule { }
+        StoreModule.forRoot({ index: indexReducer }),
+        SharedModule.forRoot(),
+    ],
+    declarations: [AppComponent, HeaderComponent, FooterComponent, SidebarComponent, ThemeCustomizerComponent, IndexComponent, AppLayout, AuthLayout],
+    providers: [Title],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
--- nfpApp/src/environments/environment.prod.ts
+++ nfpApp/src/environments/environment.prod.ts
@@ -1,14 +1,3 @@
 export const environment = {
-  production: true,
-  defaultauth: 'fackbackend',
-  firebaseConfig: {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: ''
-  }
+    production: true,
 };
